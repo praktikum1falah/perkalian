95e0e6253ba08141783ddae028ee7b473675a46f
SBC4 <SBC4@SBC10>
2015-04-28 13:06:45 +0700

    semangat yuk

---
+++ src/perkalian/pembagian2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perkalian;
+
+/**
+ *
+ * @author SBC4
+ */
+public class pembagian2 {
+     public static void main(String[] args) {
+       int a='6';
+       int b='4';
+       int c=a/b;
+        System.out.println("");
+       // System.out.println("perkalian : +a+"x"+b"= "+c);
+       // System.out.println("perkalian : "+a+"x"+b" = "+c");
+}}
