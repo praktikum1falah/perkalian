c7bd1beb1a249aa9a936ebb6ea2b2a91c15e9379
SBC4 <SBC4@SBC09>
2015-04-28 12:43:09 +0700

    ayooooo sareng-sareng belajar JAVA

---
+++ src/perkalian/Perkalian.java
@@ -0,0 +1,15 @@
+
+package perkalian;
+
+
+public class Perkalian {
+
+    public static void main(String[] args) {
+       int a='6';
+       int b='4';
+       int c=a*b;
+        System.out.println("");
+       // System.out.println("perkalian : +a+"x"+b"= "+c);
+       // System.out.println("perkalian : "+a+"x"+b" = "+c");
+    }
+}
